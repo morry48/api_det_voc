23396765b2ce1fe6c8e6ce6641d5add16f3ea592
morry48 <yyyyuuuuuuuu0408@gmail.com>
2023-02-05 15:09:59 +0900

    cors対策

--- go/src/server/server.go
+++ go/src/server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	vocabulary "nothing-behind.com/sample_gin/controller"
+	"time"
 )
 
 func Init() {
@@ -12,6 +14,34 @@ func Init() {
 
 func router() *gin.Engine {
 	r := gin.Default()
+
+	// ここからCorsの設定
+	r.Use(cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			"*",
+		},
+		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Access-Control-Allow-Origin",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: false,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	}))
 
 	v := r.Group("/vocabularies")
 	{
